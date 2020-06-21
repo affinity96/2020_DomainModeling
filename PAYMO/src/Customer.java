0f51d69f3e19d689519ab82e96b7f8e60dcf4491
이시은 <skvkffish@gmail.com>
2020-06-21 16:34:52 +0900

    customer add cart

--- PAYMO/src/Customer.java
+++ PAYMO/src/Customer.java
@@ -1,11 +1,22 @@
 public class Customer {
     int id;
-    public Customer(int id){
-        this.id=id;
+    Cart cart;
+
+    public Customer(int id) {
+        this.id = id;
+        startShopping();
     }
 
-    public int getID(){
+    public int getID() {
         return this.id;
     }
-    
+
+    public Cart getCart() {
+        return this.cart;
+    }
+
+    public void startShopping() {
+        cart = new Cart();
+
+    }
 }
