7f2bd96b540949aeb9baf5b3618388f3378925d5
이시은 <skvkffish@gmail.com>
2020-06-21 17:01:34 +0900

    add arraylist to cart

--- PAYMO/src/Cart.java
+++ PAYMO/src/Cart.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 
 public class Cart {
 	int totalPrice = 0;
-	
-	
+	// Item itemsinCart;
+	// make array list
+	ArrayList<OrderLineItem> lineItems = new ArrayList<>();
+
+	public Cart() {
+
+	}
 }
