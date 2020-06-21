d198d0f9dc7af6a7c638dc60638b5208dd8cba79
이시은 <skvkffish@gmail.com>
2020-06-21 18:43:10 +0900

    modify categotylist

--- PAYMO/src/CategoryList.java
+++ PAYMO/src/CategoryList.java
@@ -1,34 +1,36 @@
 
 public class CategoryList {
 
-	ProductCatalog[] productCatalog=new ProductCatalog[5];
+	ProductCatalog[] productCatalog = new ProductCatalog[5];
 
 	public CategoryList() {
 
-		String[] category = {"Top", "Bottom", "Accesary", "etc", "onepiece"};
+		String[] category = { "Top", "Bottom", "Accesary", "etc", "onepiece" };
 
-		for(int i=0; i<category.length; i++) {
-			ProductCatalog p = new ProductCatalog(i,category[i]);
-			productCatalog[i] =p;
+		for (int i = 0; i < category.length; i++) {
+			ProductCatalog p = new ProductCatalog(i, category[i]);
+			productCatalog[i] = p;
 		}
-
 
 	}
-	public void showItemDescription(int categoryID, int selectedItemId) {
-//		this.productCatalog.
-//			System.out.print
-			productCatalog[categoryID].getItemDescription(selectedItemId);
 
-		}
+	public void showItemDescription(int categoryID, int selectedItemId) {
+		// this.productCatalog.
+		// System.out.print
+		productCatalog[categoryID].getItemDescription(selectedItemId);
+
+	}
+
 	public void selectCategory(int categoryID) {
-//	this.productCatalog.
-//		System.out.print
+		// this.productCatalog.
+		// System.out.print
 		productCatalog[categoryID].getItemlist();
 
 	}
+
 	public void showCategory() {
-		for(int i=0; i<this.productCatalog.length; i++) {
-			System.out.print(" "+this.productCatalog[i].CatalogID+" "+this.productCatalog[i].CatalogName);
+		for (int i = 0; i < this.productCatalog.length; i++) {
+			System.out.print(" " + this.productCatalog[i].CatalogID + " " + this.productCatalog[i].CatalogName);
 		}
 		System.out.println();
 	}
